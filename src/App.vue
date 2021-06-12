3b415c00bbd13627adc34e56478f456e04a3d206
yunseonpyo <yssi365c@gmail.com>
2021-06-12 21:27:27 +0700

    vue3-webpack-template

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1> {{ message }} </h1>
+  <HelloWolrd />
+</template>
+
+<script>
+import HelloWolrd from "~/components/HelloWorld"
+
+export default {
+  components: {
+    HelloWolrd
+  },
+  data() {
+    return{
+      message : "Hello Vue!!!"
+    }
+  }
+}
+</script>
